16ff1a626dcd19ce299505c8c48b3e0d78b41824
Miguel Ángel Padilla <mapadilla@isotrol.net>
2018-12-28 14:07:47 +0100

    actualizacion branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'miProyecto';
+  nombre = 'Miguel';
+  apellido = 'Padilla';
 }
